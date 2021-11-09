0044bf54887ba6feae4ec966e9b3a8b4f75239c7
SidisLiveYT <SidisLiveYT@users.noreply.github.com>
2021-11-09 14:09:20 +0000

    deploy: 88c007463c95bdd620221e25a346a3270274f5e6

--- src/index.mjs
+++
@@ -1,6 +0,0 @@
-import YoutubeDownloader from './index.js'
-
-export default YoutubeDownloader
-export const StreamDownloader = YoutubeDownloader.StreamDownloader
-export const Extractor = YoutubeDownloader.Extractor
-export const HumanTimeConversion = YoutubeDownloader.HumanTimeConversion
